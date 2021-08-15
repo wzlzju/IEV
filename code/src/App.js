61a0946ba831d31f39ec29cd44fd727231d00791
LALBJ <905976755@qq.com>
2021-08-15 15:34:56 +0800

    construct web

--- code/src/App.js
+++ code/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Row, Col } from 'antd';
+import { flagsList, industryList } from './config/fileList'
 
 function App() {
+  const industryDir = "./assets/images/industry/"
+  const flagsDir = "./assets/images/national-flags/"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row>
+        <Col span={4}>config area</Col>
+        <Col className="content" span={16}>
+          <div className="DrawPlot">
+            <div className="title">
+              <h1>Industrial Economics Visualization</h1>
+            </div>
+            <div className="icon-display">
+              <Row>
+                <Col className="flags-display" span={12}>
+                  <Row className="flags-display">
+                    {flagsList.map((item, index) => (
+                      <div className="flags-icon">
+                        <img src={require(`${flagsDir}${item}-flag-small.png`).default} alt={`${flagsDir}${item}.png`}/>{item}
+                      </div>
+                      //<img className="industry-icon" src={require(`./assets/images/industry/icon1.png`).default} alt={`${industryDir}${item}.png`}/>
+                    ))}
+                    </Row>
+                </Col>
+                <Col span={12}>
+                  <Row className="industry-display">
+                  {industryList.map((item, index) => (
+                    <div className="industry-icon">
+                      <img src={require(`${industryDir}${item}.png`).default} alt={`${industryDir}${item}.png`}/>
+                    </div>
+                    //<img className="industry-icon" src={require(`./assets/images/industry/icon1.png`).default} alt={`${industryDir}${item}.png`}/>
+                  ))}
+                  </Row>
+                </Col>
+              </Row>
+            </div>
+            <div className="chart-display">
+              <Row>
+                <Col span={14}>
+                  <svg height="36vh" width="100%"></svg>
+                  <svg height="24vh" width="100%"></svg>
+                  <svg height="12vh" width="100%"></svg>
+                </Col>
+                <Col span={10}>
+                  <svg height="36vh" width="100%"></svg>
+                  <svg height="36vh" width="100%"></svg>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </Col>
+        <Col span={4}>excel</Col>
+      </Row>
     </div>
   );
 }
---
+++ code/src/config/fileList.js
@@ -0,0 +1,32 @@
+const flagsList = [
+    'albania',
+    'algeria',
+    'andorra',
+    'angola',
+    'antigua-and-barbuda',
+    'bangladesh',
+    'bosnia-and-herzegovina',
+    'botswana',
+    'brazil',
+    'cambodia',
+    'cameroon',
+    'canada',
+    'cape-verde',
+    'central-african-republic',
+    'chad',
+    'china',
+    'colombia',
+    'comoros',
+    'congo-democratic-republic-of-the',
+    'congo-republic-of-the'
+  ]
+
+const industryList = [
+    'icon1', 
+    'icon2', 'icon3',
+    'icon4', 'icon5',
+    'icon6', 'icon7',
+    'icon8', 'icon9',
+    'icon10'
+  ]
+export {flagsList, industryList}
